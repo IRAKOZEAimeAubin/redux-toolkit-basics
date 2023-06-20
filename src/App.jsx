9d2a889b5a313d14f576391ab7dca07ced0ba71f
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2023-06-20 12:40:12 +0200

    feat: add styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ const App = () => {
   const dispatch = useDispatch()
   
   return (
-    <div>
-      <h1>Count:</h1>
-      <span>{ count }</span>
-      <div>
-        <button onClick={ () => dispatch( increment() ) }>Increment</button>
-        <button onClick={ () => dispatch( decrement() ) }>Decrement</button>
-        <button onClick={ () => dispatch( incrementByAmount(44) ) }>Increment by 44</button>
+    <div className="flex flex-col items-center mt-4 gap-3">
+      <div className="flex items-center justify-center text-xl gap-1 font-bold rounded-sm">
+        <h1 className="tracking-wider">Count:</h1>
+        <span className="bg-orange-200 py-1 px-2 rounded-sm">{ count }</span>
+      </div>
+      <div className="flex items-center justify-center gap-4">
+        <button onClick={ () => dispatch( increment() ) } className="bg-green-300 p-2 rounded-md font-semibold">Increment</button>
+        <button onClick={ () => dispatch( decrement() ) } className="bg-red-300 p-2 rounded-md font-semibold">Decrement</button>
+        <button onClick={ () => dispatch( incrementByAmount(44) ) } className="bg-blue-300 p-2 rounded-md font-semibold">Increment by 44</button>
       </div>
     </div>
   )
